9306f1c8bb3d80c2354f65d162c98050b0e25d52
droldanb <cF0T0O6K4l>
2022-11-15 21:06:25 +0100

    [FEATURE] Rounded image list and added bottom detail.

--- app/src/main/java/com/example/jpcmesaejercicios/ui/heroes/HeroesScreen.kt
+++ app/src/main/java/com/example/jpcmesaejercicios/ui/heroes/HeroesScreen.kt
@@ -1,6 +1,7 @@
 package com.example.jpcmesaejercicios.ui.heroes
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -21,6 +23,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -51,7 +55,7 @@ fun HeroesScreen(onHeroClick: (Int) -> Unit) {
             )
         }
     ) { padding ->
-        HeroesList (
+        HeroesList(
             onHeroClick = onHeroClick
         )
         padding
@@ -64,19 +68,23 @@ fun HeroesList(viewModel: HeroesViewModel = viewModel(), onHeroClick: (Int) -> U
 
     if (state.heroes.isNotEmpty()) {
         LazyColumn {
-            items(state.heroes) { heroe ->
+            itemsIndexed(state.heroes) { index, heroe ->
                 Row(
                     Modifier
                         .height(intrinsicSize = IntrinsicSize.Max)
-                        .fillMaxWidth().clickable {
+                        .fillMaxWidth()
+                        .clickable {
                             onHeroClick(heroe.id)
                         }
+                        .padding(16.dp)
                 ) {
                     Column {
                         Image(
                             painter = rememberAsyncImagePainter(heroe.images.sm),
                             contentDescription = "*",
-                            modifier = Modifier.size(80.dp),
+                            modifier = Modifier
+                                .size(80.dp)
+                                .clip(RoundedCornerShape(8.dp)),
                             contentScale = ContentScale.FillBounds
                         )
                     }
@@ -91,6 +99,17 @@ fun HeroesList(viewModel: HeroesViewModel = viewModel(), onHeroClick: (Int) -> U
                     ) {
                         Text(text = heroe.name, fontWeight = FontWeight.Bold, fontSize = 18.sp)
                         Text(text = heroe.biography.fullName)
+                    }
+                }
+                if (index != (state.heroes.size - 1)) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(Color.Gray)
+                            .padding(0.dp, 4.dp)
+                    ) {
+                        // Nothing
                     }
                 }
             }
